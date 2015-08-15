c7e9a08b26f42d9c5ecd86ed97200115e54e0ee8
Doomhammer458 <tim.tidwell@yahoo.com>
2015-08-15 02:35:27 -0700

    init

---
+++ approvalRequest.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Request Approval Button
+// @namespace    reddit.com/r/science
+// @version      1.6
+// @description  AoS Approval Request
+// @author       glr123
+// @match        https://reddit.com/r/ScienceTest/*
+// @match		 https://reddit.com/r/science/*
+// @include      https://reddit.com/r/ScienceTest/*
+// @include		 https://reddit.com/r/science/*
+// @grant        none
+// @updateURL	 http://scienceama.com/approvalbutton.user.js
+// @downloadURL  http://scienceama.com/approvalbutton.user.js
+// ==/UserScript==
+
+$(document).ready(function(){
+    $(".pretty-button" && ".positive").html("Request Approval");
+    $(".pretty-button" && ".positive").removeAttr("onclick"); 
+    var user = $('span.user').children('a').text();
+    
+    $('a.pretty-button' && 'a.positive').click(function(event){
+       
+          var SubID = $(this).closest('ul').find('a.bylink').attr('href');
+          var post = 'New Approval Request:\n>Username - '+user+'\n>Permalink - <'+SubID+'>';
+        
+          $.ajax({
+            type: 'post',
+            context: this,
+            url: 'https://hooks.slack.com/services/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',
+              data: 'payload=' + JSON.stringify({'text': post}),
+            success: function () {
+              $(this).html("Request Sent!");
+              $(this).css("color","blue").css("background-color","#eee").css("background-image","none");
+              $(this).removeAttr("href");
+            }
+          });
+          return false;
+    });
+});
+
